c8006329f158c49325d7f7fb108e5dc7e6dded4d
andre myszko <44304680+AndreMyszko@users.noreply.github.com>
2019-09-13 18:45:39 -0300

    primeiro commit (1)

---
+++ CadastroFinal/CadastroPessoas/Aluno.cs
@@ -0,0 +1,24 @@
+﻿// # andre myszko # 2019102420
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadastroPessoas
+{ 
+    class Aluno
+    {
+        public Aluno(){}
+
+        //Atributos da classe Pessoas:
+        public string Cod { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public Aluno(string cod, string nome, int idade)
+        {
+            this.Cod = cod;
+            this.Nome = nome;
+            this.Idade = idade;
+        }
+    }
+}
---
+++ CadastroFinal/CadastroPessoas/Program.cs
@@ -0,0 +1,127 @@
+﻿// # andre myszko # 2019102420
+using System;
+using System.Collections.Generic;
+
+namespace CadastroPessoas
+{
+    class Program
+    {
+        //Método Main apresenta o MENU, aguarda seleção e separa em casos, apenas SAIR encerra o laço "do/while";
+        static void Main(string[] args)
+        {
+            //_startEnd - boolean(true/false) responsável por encerrar o loop do MENU.
+            Boolean _startEnd = true;
+            //listaCadastros - responsável por armazenar informações de cadastro (nome, idade, altura) em uma List.
+            List<Aluno> listaCadastros = new List<Aluno>();
+            do
+            {
+                Console.WriteLine("----------------------------------");
+                Console.WriteLine("MENU:\n");
+                Console.WriteLine($"1-Incluir\n2-Alterar\n3-Excluir\n4-Listar\n5-Pesquisar\n0-SAIR...");
+                Console.WriteLine("----------------------------------");
+                Console.WriteLine("Digite sua opção:\n");
+                string _option = Console.ReadLine();
+                switch (_option)
+                {
+                    //INCLUIR NA LISTA:
+                    case "1":
+                        listaCadastros.Add(Incluir());
+                        break;
+                    //ALTERAR REGISTRO NA LISTA (ID):
+                    case "2":
+                        Console.WriteLine("\nALTERAR:\n");
+                        Console.WriteLine("\nInsira CÓDIGO do registro que deseja alterar:\n");
+                        string alt = (Console.ReadLine());
+                        Aluno alterar = listaCadastros.Find(x => x.Cod == alt);
+                        if (alt != null)
+                        {
+                            Console.Write("Código: ");
+                            alterar.Cod = Console.ReadLine();
+                            Console.Write("Nome: ");
+                            alterar.Nome = Console.ReadLine();
+                            Console.Write("Idade: ");
+                            alterar.Idade = Int32.Parse(Console.ReadLine());
+                        }
+                        else
+                        {
+                            Console.WriteLine("***Registro não existe nesta lista.***");
+                        }
+                        break;
+                    //EXCLUIR DA LISTA (ID):
+                    case "3":
+                        Console.WriteLine("\nEXCLUIR:\n");
+                        Console.WriteLine("Insira o ID do registro que deseja apagar:\n");
+                        int del = Int32.Parse(Console.ReadLine());
+                        if (del <= listaCadastros.Count)
+                        {
+                            listaCadastros.RemoveAt(del);
+                        }
+                        else
+                        {
+                            Console.WriteLine("***Registro não existe nesta lista.***");
+                        }
+                        break;
+                    //MOSTRAR LISTA:
+                    case "4":
+                        Console.WriteLine("\nLISTA:\n");
+                        Console.WriteLine("ID - COD. - NOME - IDADE\n");
+                        for (int i = 0; i < listaCadastros.Count; i++)
+                        {
+                            foreach (Aluno pessoa in listaCadastros)
+                            {
+                                Console.WriteLine($"{i++} - {pessoa.Cod} - {pessoa.Nome} - {pessoa.Idade}");
+                            }
+                        }
+                        break;
+                    //PESQUISAR(ID):
+                    case "5":
+                        Console.WriteLine("\nPESQUISAR:\n");
+                        Console.WriteLine("Insira o CÓDIGO do registro que deseja pesquisar:\n");
+                        string src = Console.ReadLine();
+                        Aluno pesq = listaCadastros.Find(x => x.Cod == src);
+                        if(pesq != null)
+                        {
+                            Console.WriteLine($"Cod: {pesq.Cod}");
+                            Console.WriteLine($"Nome: {pesq.Nome}");
+                            Console.WriteLine($"Idade:{pesq.Idade}");
+                        }
+                        break;
+                    //ENCERRAR LISTA:
+                    case "0":
+                        _startEnd = false;
+                        Environment.Exit(0);
+                        break;
+                    //ERRO NA ESCOLHA DE OPÇÃO (DEFAULT)
+                    default:
+                        Console.WriteLine("\n***Opção não existe.***\nOpções válidas: 1, 2, 3, 4, 5, 0.\n");
+                        break;
+                }
+            } while (_startEnd) ;
+        }
+        //Método Incluir, ativado com OPÇÃO-1, recebe informações de cadastro (nome, idade, altura).
+        public static Aluno Incluir()
+        {
+            Aluno pessoa = new Aluno();
+            Console.Write("\nINCLUIR:\n O CÓDIGO deve conter exatamente 5 dígitos\n");
+            bool idConfig = true;
+            do
+            {
+                Console.Write("Código: ");
+                pessoa.Cod = Console.ReadLine();
+                if (pessoa.Cod.Length != 5)
+                {
+                    Console.WriteLine("***CÓDIGO fora do formato especificado (5 dígitos)***");
+                }
+                else 
+                {
+                    idConfig = false;
+                }
+            } while (idConfig);
+            Console.Write("Nome: ");
+            pessoa.Nome = Console.ReadLine(); 
+            Console.Write("Idade: ");
+            pessoa.Idade = Int32.Parse(Console.ReadLine());
+            return pessoa;
+        }
+    }
+}
